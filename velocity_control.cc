eb461125a4e1306cb46844ea000e9824e67716b8
DaeganBrown <149893841+DaeganBrown@users.noreply.github.com>
2025-09-26 16:35:29 -0500

    why

--- velocity_control.cc
+++ velocity_control.cc
@@ -26,7 +26,7 @@ namespace gazebo
       ignition::math::Pose3d currentPose = this->link->WorldPose();
       ignition::math::Quaterniond orientation = currentPose.Rot();
       // local velocity
-      ignition::math::Vector3d localVel=(51, 0, 0);
+      ignition::math::Vector3d localVel={51, 0, 0};
       //find global transform
       ignition::math::Vector3d worldVel = orientation.RotateVector(localVel);
 
